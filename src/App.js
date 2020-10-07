ba815ceb3e7b15e66740c7badda8e6b9e5953897
Dmitry Valuev <dmitry.valuev@gmail.com>
2020-10-07 16:44:14 +0300

    added expose port 80

--- src/App.js
+++ src/App.js
@@ -3,24 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit 222 <code>src/App.js</code> and save to reload.
-				</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn react
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>This is a Docker CI demo app!</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn react
 				</a>
-      </header>
-    </div>
-  );
+			</header>
+		</div>
+	);
 }
 
 export default App;
